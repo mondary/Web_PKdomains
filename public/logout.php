487c3835d20ca6eb1a3735052b3718e6a3e67172
cmondary <clement.mondary@gmail.com>
2026-01-27 17:11:13 +0100

    fix : logout

--- public/logout.php
+++ public/logout.php
@@ -2,5 +2,12 @@
 session_start();
 session_unset();
 session_destroy();
-header("Location: " . url_for($config, "index.php"));
+
+$script = $_SERVER["SCRIPT_NAME"] ?? "";
+$dir = rtrim(str_replace("\\", "/", dirname($script)), "/");
+if (substr($dir, -7) === "/public") {
+    $dir = substr($dir, 0, -7);
+}
+$target = ($dir === "" ? "" : $dir) . "/index.php";
+header("Location: " . $target);
 exit;
